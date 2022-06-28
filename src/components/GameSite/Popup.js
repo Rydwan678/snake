3c46c7a5b499579fc1aa37225cc2f96ffd656e3c
Adam Rydwański <103386462+Rydwan678@users.noreply.github.com>
2022-06-28 19:56:42 +0200

    Update

--- src/components/GameSite/Popup.js
+++ src/components/GameSite/Popup.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default function Popup(props) {
+  const styles = {
+    background:
+      props.type === "lose"
+        ? "linear-gradient(90deg, #EB3349 0%, #F45C43 51%, #EB3349  100%)"
+        : "linear-gradient(90deg, #9ebd13 0%, #008552 100%)",
+  };
+
   if (props.type === "gamePaused")
     return (
       <div className="popup-body">
-        <div className="popup">
+        <div className="popup" styles={styles}>
           <p>GAME PAUSED</p>
         </div>
       </div>
@@ -12,7 +19,7 @@ export default function Popup(props) {
   if (props.type === "lose")
     return (
       <div className="popup-body">
-        <div className="popup">
+        <div className="popup" styles={styles}>
           <p>YOU LOST</p>
         </div>
       </div>
